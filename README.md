cb268b10e0b5f70a4071d12e32e3921834203f9c
YahyaHooman <YahyaHooman@gmail.com>
2023-04-20 21:42:40 +0430

    Third Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Visual-Programming-1
-This repository includes C# simple programs including class activities, homeworks and some  more simple projects. 
+This repository includes C# simple programs including class activities, homeworks and some more simple projects.
+and has two branches, the main branch and the master branch you can find out all the codes and activities in the master brouch if willing!
---
+++ Week_2/Variables/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // this is the class ativity code and simply shows the different types of string concatenation.
+            //or string formating.
+            double age=0.0;
+            string name = "", message;
+
+            Console.Write("Your Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Your Age: ");
+            age = Convert.ToDouble(Console.ReadLine());
+
+            message = "Your name is " + name + " and you are " + age + " years old!";
+            Console.Write("Printing the text with type 1: ");
+            Console.WriteLine(message);
+
+            Console.Write("Printing the text with type 2: ");
+            Console.WriteLine("Your name is " + name + " and you are " + age + " years old!");
+
+            Console.Write("Printing the text with type 3: ");
+            Console.WriteLine("Your name is {0} and you are {1} years old!", name, age);
+
+            Console.Write("Printing the text with type 4: ");
+            Console.WriteLine($"Your name is {name} and you are {age} years old!");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
